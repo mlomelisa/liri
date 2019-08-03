420a462db9bc27c05973d1fc599af3ba0e45a895
mlomelisa <monica.lomelim@gmail.com>
2019-08-03 15:41:35 -0700

    Spotify song default included The Sign

--- liri.js
+++ liri.js
@@ -4,31 +4,14 @@ var keys = require("./keys.js");
 var axios = require('axios');
 var moment = require('moment');
 var Spotify = require('node-spotify-api');
-
-var SpotifyWebApi = require('spotify-web-api-node');
-
-// var spotifyApi = new SpotifyWebApi();
-
+var fs = require("fs");
 var action = process.argv[2];
-var entryArray = []
 
-// Function concatenate entry from Arguments 3 in advance.
-
-var ConcatenateEntry = function(){
-  var entry = process.argv;
- 
-  for (let j=3; j<entry.length ; j++){
-    entryArray.push(entry[j]);
-  }
- 
-  return entryArray;
-}
 
 // Function ConcertThis
 var concertThis = function() {
-  var artist = ConcatenateEntry();
   
-  artist = artist.join('%20');
+  artist = process.argv.slice(3).join('%20');
   
  axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&date=upcoming").then(
    function(response) {
@@ -47,39 +30,57 @@ var concertThis = function() {
 
 
 // Function Spotify This Song
- var spotifyThisSong = function() {
-  // var spotifyApi = new SpotifyWebApi(keys.spotify); 
-     var spotify = new Spotify(keys.spotify);
-   var song = ConcatenateEntry();
-   song = song.join(' ');
+
+ var spotifyThisSong = function(song) {
+  var song = process.argv.slice(3).join(' ');
+
+   var spotify = new Spotify(keys.spotify);
   
-  spotify.search({type: 'track', query: song, limit: 4})
+   var songSearch = function(song){
+   
+       spotify.search({type: 'track', query: song, limit: 1})
       .then(function(response) {
         
-         var artist_name = response.tracks.items[0].artists[0].name;
-         var song_name = response.tracks.items[0].name;
-         var link_spotify = response.tracks.items[0].external_urls.spotify;
-         var album_name = response.tracks.items[0].album.name;
-        
-         console.log("\n")
-         console.log('Artist(s): ' + artist_name);
-         console.log('Song: ' + song_name);
-         console.log('Link: ' + link_spotify);
-         console.log('Album: ' + album_name)
-         console.log("\n")
+        for (let i =0; i < response.tracks.items.length; i++){
+          
+          var artist_name = response.tracks.items[i].artists[0].name;
+          var song_name = response.tracks.items[i].name;
+          var link_spotify = response.tracks.items[i].external_urls.spotify;
+          var album_name = response.tracks.items[i].album.name;
+
+          console.log("Item: " + i)
+          console.log('Artist(s): ' + artist_name);
+          console.log('Song: ' + song_name);
+          console.log('Link: ' + link_spotify);
+          console.log('Album: ' + album_name)
+          console.log("\n")
+                
+        }
+         
        })
        .catch(function(err) {
           console.log(err);
    
         });  
+      }
+   
+   if (song.length === 0) {
+     
+    song = ['the','sign'];
+    song = ('"' + song.join(' ') + '"');
+    songSearch(song);
+
+   } else { 
+    song = ('"' + song + '"'); 
+  songSearch(song);
+
   }
+ }
+
 // Function movieThis
-
-var movieThis = function(){
-  var movie = ConcatenateEntry();
-  
-  movie = movie.join('+');
-
+var movieThis = function(){ 
+  var movie = process.argv.slice(3).join('+');
+ 
   axios.get("http://www.omdbapi.com/?t="+movie+"&y=&plot=short&apikey=trilogy").then(
   function(response) {
     // Then we print out the imdbRating
@@ -93,12 +94,52 @@ var movieThis = function(){
     console.log("Plot: " + response.data.Plot)
     console.log("Actors: " + response.data.Actors)
     console.log("\n")
-
-    
- 
+   
   }
 );
 
+}
+
+// Function Do what it says
+
+var doWhatItSays = function() {
+
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+  
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+  
+    // We will then re-display the content as an array for later use.
+    console.log(dataArr);
+    entry = dataArr[1];
+    action = dataArr[0];
+    
+    switch (action) {
+      case 'concert-this':
+          concertThis(entry);
+      break;
+      case 'spotify-this-song':
+          spotifyThisSong(entry);
+      break;
+      case 'movie-this':
+          movieThis();
+      break;
+      case 'do-what-it-says':
+          doWhatItSays();
+      break;
+    
+      default:
+        console.log('This option is not valid')
+    
+    }
+  
+  });
+  
 }
 
 
@@ -122,4 +163,6 @@ switch (action) {
     console.log('This option is not valid')
 
 }
+
+// spotifyThisSong('The Sign')
 
